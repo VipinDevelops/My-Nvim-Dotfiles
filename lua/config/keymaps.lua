04be86d1652ce115797320cf01a5a991cc9f61bb
VipinDevelops <VipinChaudhary1809@gmail.com>
2024-01-07 14:18:01 +0530

    Basic confg without error

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,29 +17,28 @@ local opts = { noremap = true, silent = true }
 keymap.set("n", "<C-a>", "gg<S-v>G")
 
 --- Jumplist
-keymap.set("n", "<C-m>", "<C-i>", opts)
+-- keymap.set("n", "<C-m>", "<C-i>", opts)
 
---- New tab
-keymap.set("n", "te", ":tabedit<Return>", opts)
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- keymap.set("n", "te", ":tabedit<Return>", opts)
+-- keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+-- keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
 
 --- Move window
-keymap.set("n", "sh", "<C-w>h")
-keymap.set("n", "sk", "<C-w>k")
-keymap.set("n", "sj", "<C-w>j")
-keymap.set("n", "sl", "<C-w>l")
-
+-- keymap.set("n", "sh", "<C-w>h")
+-- keymap.set("n", "sk", "<C-w>k")
+-- keymap.set("n", "sj", "<C-w>j")
+-- keymap.set("n", "sl", "<C-w>l")
+--
 --- Resize window
-keymap.set("n", "<C-w><left>", "<C-w><")
-keymap.set("n", "<C-w><right>", "<C-w>>")
-keymap.set("n", "<C-w><up>", "<C-w>+")
-keymap.set("n", "<C-w><down>", "<C-w>-")
-
+-- keymap.set("n", "<C-w><left>", "<C-w><")
+-- keymap.set("n", "<C-w><right>", "<C-w>>")
+-- keymap.set("n", "<C-w><up>", "<C-w>+")
+-- keymap.set("n", "<C-w><down>", "<C-w>-")
+--
 --- Diagonstics
 keymap.set("n", "<C-j>", function()
   vim.diagnostic.goto_next()
@@ -47,4 +46,3 @@ end, opts)
 
 -- Comments
 vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
-vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
